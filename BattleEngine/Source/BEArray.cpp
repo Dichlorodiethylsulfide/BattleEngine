acc0968821b53cb952bea99824a74676850acc2b
riki <rikilowe500@btinternet.com>
2024-03-04 22:23:54 +0000

    clean the entire project
    remove all files

--- BattleEngine/Source/BEArray.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "BEArray.h"
-
-BETrackedAllocation* allocations = new BETrackedAllocation[ALLOC_COUNT];
-size_t totalAllocations = 0;
-size_t totalDeallocations = 0;
--- BigBattle/BigBattle.cpp
+++ BigBattle/BigBattle.cpp
@@ -1,38 +1,5 @@
 
-// Based on Touhou Big Big Battle
-
-#include <BEConsoleIO.h>
-#include <BEWindow.h>
-
-#include <BEVector.h>
-#include <BEList.h>
-#include <iostream>
-
-#define NOT !
-
-static void ValidateAllocations()
-{
-    bool validated = true;
-    for(size_t i = 0; i < ALLOC_COUNT; i++)
-    {
-        if(NOT allocations[i].is_free)
-        {
-            validated = false;
-            std::cout << "Allocation was not freed at: " << i << "\n";
-        }
-    }
-    if(validated)
-    {
-        std::cout << "Successfully validated allocations" << "\n";
-    }
-    std::cout << "Total Allocations: " << totalAllocations << "\n";
-    std::cout << "Total Deallocations: " << totalDeallocations << "\n";
-}
-
-void function()
-{
-    BEVector<int> values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-}
+// Based on Touhou Big Big Battle + Unreal Engine
 
 int main(int argc, char* argv[])
 {
@@ -42,12 +9,5 @@ int main(int argc, char* argv[])
     window.EnterMainLoop();
     BEWindow::Quit();
     DEBUG("Goodbye World!");*/
-
-    for(size_t i = 0; i < 10; i++)
-    {
-        function();
-    }
-    
-    ValidateAllocations();
     return 0;
 }
